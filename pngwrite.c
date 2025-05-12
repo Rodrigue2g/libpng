29c21fc4baba5090f3cc70b31a02ad3ca52cd6b5
Rodrigue de Guerre <99824035+Rodrigue2g@users.noreply.github.com>
2025-05-12 11:45:06 +0200

    Update pngwrite.c

--- pngwrite.c
+++ pngwrite.c
@@ -194,7 +194,10 @@ png_write_info_before_PLTE(png_structrp png_ptr, png_const_inforp info_ptr)
 
 #  ifdef PNG_WRITE_iCCP_SUPPORTED /* Priority 3 */
          if ((info_ptr->valid & PNG_INFO_iCCP) != 0)
-         {
+         {  
+            /*--- Old Version ---*/
+            // png_write_iCCP(png_ptr, info_ptr->iccp_name, info_ptr->iccp_profile);
+            /*--- New (fixed) Version ---*/
             png_write_iCCP(png_ptr, info_ptr->iccp_name,
                 info_ptr->iccp_profile, info_ptr->iccp_proflen);
          }
