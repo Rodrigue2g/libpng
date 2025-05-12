3bd6ab9beba36610bf41da517805c7aff63d4e6b
Rodrigue de Guerre <99824035+Rodrigue2g@users.noreply.github.com>
2025-05-12 11:55:27 +0200

    Update pngpriv.h

--- pngpriv.h
+++ pngpriv.h
@@ -1257,17 +1257,16 @@ PNG_INTERNAL_FUNCTION(void,png_write_eXIf,(png_structrp png_ptr,
 
 #ifdef PNG_WRITE_iCCP_SUPPORTED
 /*--- Old Version ---*/
-// PNG_INTERNAL_FUNCTION(void,png_write_iCCP,(png_structrp png_ptr,
-//    png_const_charp name, png_const_bytep profile), PNG_EMPTY);
+PNG_INTERNAL_FUNCTION(void,png_write_iCCP,(png_structrp png_ptr,
+   png_const_charp name, png_const_bytep profile), PNG_EMPTY);
    /* The profile must have been previously validated for correctness, the
     * length comes from the first four bytes.  Only the base, deflate,
     * compression is supported.
     */
-/*--- Old Version eof ---*/
 /*--- New (fixed) Version ---*/
-PNG_INTERNAL_FUNCTION(void,png_write_iCCP,(png_structrp png_ptr,
-   png_const_charp name, png_const_bytep profile, png_uint_32 proflen),
-   PNG_EMPTY);
+// PNG_INTERNAL_FUNCTION(void,png_write_iCCP,(png_structrp png_ptr,
+//    png_const_charp name, png_const_bytep profile, png_uint_32 proflen),
+//    PNG_EMPTY);
    /* Writes a previously 'set' profile.  The profile argument is **not**
     * compressed.
     */
