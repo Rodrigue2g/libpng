7da801a7b1853daecaed4ac30520309a47fc064c
Rodrigue de Guerre <99824035+Rodrigue2g@users.noreply.github.com>
2025-05-13 19:11:41 +0200

    Update libpng_write_fuzzer.cc

--- contrib/oss-fuzz/libpng_write_fuzzer.cc
+++ contrib/oss-fuzz/libpng_write_fuzzer.cc
@@ -8,6 +8,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 #include <vector>
 #include <fstream>
@@ -15,26 +16,6 @@
 
 #define PNG_INTERNAL
 #include "png.h"
-
-/*
-#define PNG_CLEANUP \
-  if(png_handler.png_ptr) \
-  { \
-    if (png_handler.row_ptr) \
-      png_free(png_handler.png_ptr, png_handler.row_ptr); \
-    if (png_handler.info_ptr) { \
-      png_destroy_read_struct (&png_handler.png_ptr, &png_handler.info_ptr, nullptr); \
-      png_destroy_write_struct(&png_handler.png_ptr, &png_handler.info_ptr); \
-    } \
-    else { \
-      png_destroy_read_struct (&png_handler.png_ptr, nullptr, nullptr); \
-      png_destroy_write_struct(&png_handler.png_ptr, nullptr); \
-    } \
-    png_handler.png_ptr = nullptr; \
-    png_handler.row_ptr = nullptr; \
-    png_handler.info_ptr = nullptr; \
-  }
-*/
   
 #define PNG_CLEANUP \
   if(png_handler.png_ptr) \
@@ -78,38 +59,6 @@
 typedef unsigned char   uch;
 typedef unsigned short  ush;
 typedef unsigned long   ulg;
-
-
-typedef struct _mainprog_info {
-    double gamma;
-    long width;
-    long height;
-    time_t modtime;
-    FILE *infile;
-    FILE *outfile;
-    void *png_ptr;
-    void *info_ptr;
-    uch *image_data;
-    uch **row_pointers;
-    char *title;
-    char *author;
-    char *desc;
-    char *copyright;
-    char *email;
-    char *url;
-    int filter;    /* command-line-filter flag, not PNG row filter! */
-    int pnmtype;
-    int sample_depth;
-    int interlaced;
-    int have_bg;
-    int have_time;
-    int have_text;
-    jmp_buf jmpbuf;
-    uch bg_red;
-    uch bg_green;
-    uch bg_blue;
-} mainprog_info;
-
 
 struct WriteBuffer {
   std::vector<uint8_t> data;
@@ -227,6 +176,9 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
     PNG_COLOR_TYPE_RGBA
   };
   int color_type = color_type_options[data[5] % 4];
+
+  png_color_8 shift = {bit_depth / 2, bit_depth / 2, bit_depth / 2, bit_depth / 2, bit_depth / 2};
+  uint8_t flags = data[6];
   
   PngObjectHandler png_handler;
   png_handler.write_buf = new WriteBuffer();
@@ -247,29 +199,70 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
     return 0;
   }
 
-  png_color_8 shift = {bit_depth / 2, bit_depth / 2, bit_depth / 2, bit_depth / 2, bit_depth / 2};
-  uint8_t flags = data[6];
+  /* make sure outfile is (re)opened in BINARY mode */
+  // png_init_io(png_ptr, mainprog_ptr->outfile);
+
+  png_set_compression_level(png_ptr, Z_BEST_COMPRESSION);
 
 
-  /* limit allocations & hook up in-memory write */
-  png_set_mem_fn(png_handler.png_ptr, nullptr, limited_malloc, limited_free);
-  png_set_write_fn(png_handler.png_ptr, png_handler.write_buf,
-                   write_cb, flush_cb);
+  // /* limit allocations & hook up in-memory write */  -- Not sure about these two?
+  // png_set_mem_fn(png_handler.png_ptr, nullptr, limited_malloc, limited_free);
+  // png_set_write_fn(png_handler.png_ptr, png_handler.write_buf,
+  //                  write_cb, flush_cb);
   
   png_set_IHDR(png_handler.png_ptr, png_handler.info_ptr,
                width, height,
-               /* bit depth */ 8,
-               PNG_COLOR_TYPE_RGBA,
+               bit_depth, /* bit depth  8, */
+               color_type,
                PNG_INTERLACE_NONE,
                PNG_COMPRESSION_TYPE_DEFAULT,
                PNG_FILTER_TYPE_DEFAULT);
 
-  if (flags & 1) png_set_packing(png_handler.png_ptr);
-  if (flags & 2) png_set_shift(png_handler.png_ptr, &shift);
-  if (flags & 4) png_set_swap_alpha(png_handler.png_ptr);
-  if (flags & 8) png_set_invert_alpha(png_handler.png_ptr);
+  /*---- Set the gamma component of the png ----*/
+  png_set_gAMA(png_ptr, info_ptr, mainprog_ptr->gamma);
+
+  /* we know it's RGBA, not gray+alpha */
+  png_color_16  background;
+  background.red = 255;
+  background.green = 255;
+  background.blue = 255;
+  png_set_bKGD(png_ptr, info_ptr, &background);
+
   
-  png_write_info(png_handler.png_ptr, png_handler.info_ptr);
+  /*---- Set the time of the png ----*/
+  png_time  modtime;
+  png_convert_from_time_t(&modtime, time(NULL));
+  png_set_tIME(png_ptr, info_ptr, &modtime);
+
+  
+  /*---- Set the text of the png ----*/
+  png_text  text[6];
+  int  num_text = 6;
+  text[num_text].compression = PNG_TEXT_COMPRESSION_NONE;
+  text[num_text].key = "Title";
+  text[num_text].text = "Test image";
+
+  text[num_text].compression = PNG_TEXT_COMPRESSION_NONE;
+  text[num_text].key = "Author";
+  text[num_text].text = "Rodrigue2g;
+  
+  text[num_text].compression = PNG_TEXT_COMPRESSION_NONE;
+  text[num_text].key = "Description";
+  text[num_text].text = "Png test description for OSS-Fuzz";
+  
+  text[num_text].compression = PNG_TEXT_COMPRESSION_NONE;
+  text[num_text].key = "Copyright";
+  text[num_text].text = "2025 - All rights reserved.";
+  
+  text[num_text].compression = PNG_TEXT_COMPRESSION_NONE;
+  text[num_text].key = "E-mail";
+  text[num_text].text = "email@example.com";
+  
+  text[num_text].compression = PNG_TEXT_COMPRESSION_NONE;
+  text[num_text].key = "URL";
+  text[num_text].text = "https://www.example.com";
+  png_set_text(png_ptr, info_ptr, text, num_text);
+
 
   // png_set_write_fn(png_handler.png_ptr, png_handler.write_buf, user_write_data, user_flush_data);
   // png_set_IHDR(png_handler.png_ptr, png_handler.info_ptr, width, height,
@@ -278,6 +271,21 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
 
   // png_write_info(png_handler.png_ptr, png_handler.info_ptr);
 
+  
+  /* write all chunks up to (but not including) first IDAT */
+  png_write_info(png_handler.png_ptr, png_handler.info_ptr);
+
+  
+  /* set up the transformations:  for now, just pack low-bit-depth pixels
+   * into bytes (one, two or four pixels per byte) */
+  png_set_packing(png_handler.png_ptr);
+  // if (flags & 1) png_set_packing(png_handler.png_ptr);
+  if (flags & 2) png_set_shift(png_handler.png_ptr, &shift);
+  if (flags & 4) png_set_swap_alpha(png_handler.png_ptr);
+  if (flags & 8) png_set_invert_alpha(png_handler.png_ptr);
+
+
+
   /* Write the png image */
   size_t rowbytes = png_get_rowbytes(png_handler.png_ptr, png_handler.info_ptr);
   std::vector<uint8_t> img(rowbytes * height, 0); /* black */
@@ -285,7 +293,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   for (size_t i = 0; i < height; ++i) rows[i] = img.data() + i * rowbytes;
 
   png_write_image(png_handler.png_ptr, rows.data());
-  png_write_end(png_handler.png_ptr, nullptr);
+  // png_write_end(png_handler.png_ptr, nullptr);
 
   
   /* Write the png image in a different fation now (with png_write_row) */
@@ -297,9 +305,14 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
     }
   }
 
+  png_write_rows(png_handler.png_ptr, rows);
+  // png_write_end(png_handler.png_ptr, nullptr);
+
   for (size_t i = 0; i < height; ++i)
     png_write_row(png_handler.png_ptr, rows[i]);
 
+  /* close out PNG file; if we had any text or time info to write after
+   * the IDATs, second argument would be info_ptr: */
   png_write_end(png_handler.png_ptr, nullptr);
 
   /* ------------------------------------------------------------------ */
@@ -338,203 +351,6 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   PNG_CLEANUP
   return 0;
 }
-
-
-// extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
-//     if (size < 16) {
-//       return 0; // Not enough data to construct even minimal header.
-//     }
-  
-//     // Use some of the input data to define PNG parameters.
-//     uint32_t width = (data[0] << 8) | data[1];
-//     uint32_t height = (data[2] << 8) | data[3];
-//     int bit_depth = (data[4] % 4 + 1) * 8;  // 8, 16, 24, 32
-//     int color_type = data[5] % 6;          // 0-6 valid color types
-//     int interlace_type = data[6] % 2;
-//     int compression_type = PNG_COMPRESSION_TYPE_BASE;
-//     int filter_type = PNG_FILTER_TYPE_BASE;
-  
-//     // Limit dimensions to avoid excessive memory usage
-//     if (width == 0 || height == 0 || width > 4096 || height > 4096)
-//       return 0;
-  
-//     PngObjectHandler png_handler;
-//     png_handler.png_ptr = nullptr;
-//     png_handler.row_ptr = nullptr;
-//     png_handler.info_ptr = nullptr;
-//     png_handler.end_info_ptr = nullptr;
-
-
-//     mainprog_info *mainprog_ptr;
-
-//     mainprog_ptr->infile = &data;
-  
-//     png_structp  png_ptr;       /* note:  temporary variables! */
-//     png_infop  info_ptr;
-//     int color_type, interlace_type;
-
-
-//     /* could also replace libpng warning-handler (final NULL), but no need: */
-
-//     png_ptr = png_create_write_struct(png_get_libpng_ver(NULL), mainprog_ptr, writepng_error_handler, NULL);
-//     if (!png_ptr)
-//         return 4;   /* out of memory */
-
-//     info_ptr = png_create_info_struct(png_ptr);
-//     if (!info_ptr) {
-//         png_destroy_write_struct(&png_ptr, NULL);
-//         return 4;   /* out of memory */
-//     }
-
-
-//     /* setjmp() must be called in every function that calls a PNG-writing
-//      * libpng function, unless an alternate error handler was installed--
-//      * but compatible error handlers must either use longjmp() themselves
-//      * (as in this program) or some other method to return control to
-//      * application code, so here we go: */
-
-//     if (setjmp(mainprog_ptr->jmpbuf)) {
-//         png_destroy_write_struct(&png_ptr, &info_ptr);
-//         return 2;
-//     }
-
-
-//     /* make sure outfile is (re)opened in BINARY mode */
-
-//     png_init_io(png_ptr, mainprog_ptr->outfile);
-
-
-//     /* set the compression levels--in general, always want to leave filtering
-//      * turned on (except for palette images) and allow all of the filters,
-//      * which is the default; want 32K zlib window, unless entire image buffer
-//      * is 16K or smaller (unknown here)--also the default; usually want max
-//      * compression (NOT the default); and remaining compression flags should
-//      * be left alone */
-
-//     png_set_compression_level(png_ptr, Z_BEST_COMPRESSION);
-// /*
-//     >> this is default for no filtering; Z_FILTERED is default otherwise:
-//     png_set_compression_strategy(png_ptr, Z_DEFAULT_STRATEGY);
-//     >> these are all defaults:
-//     png_set_compression_mem_level(png_ptr, 8);
-//     png_set_compression_window_bits(png_ptr, 15);
-//     png_set_compression_method(png_ptr, 8);
-//  */
-
-
-//     /* set the image parameters appropriately */
-
-//     if (mainprog_ptr->pnmtype == 5)
-//         color_type = PNG_COLOR_TYPE_GRAY;
-//     else if (mainprog_ptr->pnmtype == 6)
-//         color_type = PNG_COLOR_TYPE_RGB;
-//     else if (mainprog_ptr->pnmtype == 8)
-//         color_type = PNG_COLOR_TYPE_RGB_ALPHA;
-//     else {
-//         png_destroy_write_struct(&png_ptr, &info_ptr);
-//         return 11;
-//     }
-
-//     interlace_type = mainprog_ptr->interlaced? PNG_INTERLACE_ADAM7 :
-//                                                PNG_INTERLACE_NONE;
-
-//     png_set_IHDR(png_ptr, info_ptr, mainprog_ptr->width, mainprog_ptr->height,
-//       mainprog_ptr->sample_depth, color_type, interlace_type,
-//       PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
-
-//     if (mainprog_ptr->gamma > 0.0)
-//         png_set_gAMA(png_ptr, info_ptr, mainprog_ptr->gamma);
-
-//     if (mainprog_ptr->have_bg) {   /* we know it's RGBA, not gray+alpha */
-//         png_color_16  background;
-
-//         background.red = mainprog_ptr->bg_red;
-//         background.green = mainprog_ptr->bg_green;
-//         background.blue = mainprog_ptr->bg_blue;
-//         png_set_bKGD(png_ptr, info_ptr, &background);
-//     }
-
-//     if (mainprog_ptr->have_time) {
-//         png_time  modtime;
-
-//         png_convert_from_time_t(&modtime, mainprog_ptr->modtime);
-//         png_set_tIME(png_ptr, info_ptr, &modtime);
-//     }
-
-//     if (mainprog_ptr->have_text) {
-//         png_text  text[6];
-//         int  num_text = 0;
-
-//         if (mainprog_ptr->have_text & TEXT_TITLE) {
-//             text[num_text].compression = PNG_TEXT_COMPRESSION_NONE;
-//             text[num_text].key = "Title";
-//             text[num_text].text = mainprog_ptr->title;
-//             ++num_text;
-//         }
-//         if (mainprog_ptr->have_text & TEXT_AUTHOR) {
-//             text[num_text].compression = PNG_TEXT_COMPRESSION_NONE;
-//             text[num_text].key = "Author";
-//             text[num_text].text = mainprog_ptr->author;
-//             ++num_text;
-//         }
-//         if (mainprog_ptr->have_text & TEXT_DESC) {
-//             text[num_text].compression = PNG_TEXT_COMPRESSION_NONE;
-//             text[num_text].key = "Description";
-//             text[num_text].text = mainprog_ptr->desc;
-//             ++num_text;
-//         }
-//         if (mainprog_ptr->have_text & TEXT_COPY) {
-//             text[num_text].compression = PNG_TEXT_COMPRESSION_NONE;
-//             text[num_text].key = "Copyright";
-//             text[num_text].text = mainprog_ptr->copyright;
-//             ++num_text;
-//         }
-//         if (mainprog_ptr->have_text & TEXT_EMAIL) {
-//             text[num_text].compression = PNG_TEXT_COMPRESSION_NONE;
-//             text[num_text].key = "E-mail";
-//             text[num_text].text = mainprog_ptr->email;
-//             ++num_text;
-//         }
-//         if (mainprog_ptr->have_text & TEXT_URL) {
-//             text[num_text].compression = PNG_TEXT_COMPRESSION_NONE;
-//             text[num_text].key = "URL";
-//             text[num_text].text = mainprog_ptr->url;
-//             ++num_text;
-//         }
-//         png_set_text(png_ptr, info_ptr, text, num_text);
-//     }
-
-
-//     /* write all chunks up to (but not including) first IDAT */
-
-//     png_write_info(png_ptr, info_ptr);
-
-
-//     /* if we wanted to write any more text info *after* the image data, we
-//      * would set up text struct(s) here and call png_set_text() again, with
-//      * just the new data; png_set_tIME() could also go here, but it would
-//      * have no effect since we already called it above (only one tIME chunk
-//      * allowed) */
-
-
-//     /* set up the transformations:  for now, just pack low-bit-depth pixels
-//      * into bytes (one, two or four pixels per byte) */
-
-//     png_set_packing(png_ptr);
-// /*  png_set_shift(png_ptr, &sig_bit);  to scale low-bit-depth values */
-
-
-//     /* make sure we save our pointers for use in writepng_encode_image() */
-
-//     mainprog_ptr->png_ptr = png_ptr;
-//     mainprog_ptr->info_ptr = info_ptr;
-
-
-//     PNG_CLEANUP
-//     return 0;
-// }
-
-
 
 
 
